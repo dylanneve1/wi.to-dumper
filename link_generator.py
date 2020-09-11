c70cfb7d5ac8c95051467c091b13552c2f99b2c6
Dylan Neve <dylanneve1@gmail.com>
2020-09-11 17:13:08 +0100

    Add in link validator and check if link is dead or not

--- link_generator.py
+++ link_generator.py
@@ -1,5 +1,7 @@
 import requests, bs4, webbrowser, re, string, random
 from selenium import webdriver
+import validators
+import urllib.request
 
 letters = string.ascii_lowercase
 digits = string.digits
@@ -7,3 +9,6 @@ random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
 url = ('https://wi.to/' + random_code)
 
 print(url)
+print (validators.url(url))
+
+print(urllib.request.urlopen(url).getcode())
