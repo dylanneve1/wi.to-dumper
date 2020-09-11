d89d79c66ec0a973ede6ff67bef094ee6d666478
Dylan Neve <dylanneve1@gmail.com>
2020-09-11 17:29:28 +0100

    Update link_generator.py

--- link_generator.py
+++ link_generator.py
@@ -1,19 +1,23 @@
 import requests, bs4, webbrowser, re, string, random, urllib.request, validators, repeat, ptimer
 from selenium import webdriver
 
-for i in range(0,500):
-    try:
-        # Generate URL
-        letters = string.ascii_lowercase
-        digits = string.digits
-        random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
-        url = ('https://wi.to/' + random_code)
+# Variables
+letters = string.ascii_lowercase
+digits = string.digits
+
+for i in range(500):
+    while True:
+        try:
+            # Generate URL
+            random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
+            url = ('https://wi.to/' + random_code)
         
-        # Print URL and validate it
-        print(url)
-        print (validators.url(url))
-        print(urllib.request.urlopen(url).getcode())
-        
-    except:
-        # If URL is dead repeat until working URL is found
-        repeat
+            # Print URL and validate it
+            print(url)
+            print (validators.url(url))
+            print(urllib.request.urlopen('https://google.com').getcode())
+            
+        except Exception: # Replace Exception with something more specific.
+            continue
+        else:
+            break
