ed6725e05cb05b60ac7c34a1878a036d53016037
Dylan Neve <dylanneve1@gmail.com>
2020-09-11 17:21:55 +0100

    Format

--- link_generator.py
+++ link_generator.py
@@ -8,11 +8,12 @@ for i in range(0,500):
         digits = string.digits
         random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
         url = ('https://wi.to/' + random_code)
-
+        
         # Print URL and validate it
         print(url)
         print (validators.url(url))
         print(urllib.request.urlopen(url).getcode())
+        
     except:
         # If URL is dead repeat until working URL is found
         repeat
