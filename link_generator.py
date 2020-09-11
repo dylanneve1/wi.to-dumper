7882fa6f103fd95a68374edca568363beff29ee6
Dylan Neve <dylanneve1@gmail.com>
2020-09-11 16:58:41 +0100

    Update and add working url generator

--- link_generator.py
+++ link_generator.py
@@ -1,8 +1,9 @@
-import random
-import string
+import requests, bs4, webbrowser, re, string, random
+from selenium import webdriver
 
 letters = string.ascii_lowercase
 digits = string.digits
 random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
+url = ('https://wi.to/' + random_code)
 
-print ('https://wi.to/' + random_code)
+print(url)
