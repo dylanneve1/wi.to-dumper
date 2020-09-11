2009efcd73f93563ebaa02fd7b618c6237360b4b
Dylan Neve <dylanneve1@gmail.com>
2020-09-11 16:46:55 +0100

    Add initial code

--- link_generator.py
+++ link_generator.py
@@ -1,1 +1,8 @@
+import random
+import string
 
+letters = string.ascii_lowercase
+digits = string.digits
+random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
+
+print ('https://wi.to/' + random_code)
