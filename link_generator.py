39dcfec5ba9fec2f86076bf51ae2c1deefd23e5d
Dylan Neve <dylanneve1@gmail.com>
2020-09-11 17:21:17 +0100

    Start work at making a loop until working link is found

--- link_generator.py
+++ link_generator.py
@@ -1,14 +1,18 @@
-import requests, bs4, webbrowser, re, string, random
+import requests, bs4, webbrowser, re, string, random, urllib.request, validators, repeat, ptimer
 from selenium import webdriver
-import validators
-import urllib.request
 
-letters = string.ascii_lowercase
-digits = string.digits
-random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
-url = ('https://wi.to/' + random_code)
+for i in range(0,500):
+    try:
+        # Generate URL
+        letters = string.ascii_lowercase
+        digits = string.digits
+        random_code = ( ''.join(random.choice(letters + digits) for i in range(16)) )
+        url = ('https://wi.to/' + random_code)
 
-print(url)
-print (validators.url(url))
-
-print(urllib.request.urlopen(url).getcode())
+        # Print URL and validate it
+        print(url)
+        print (validators.url(url))
+        print(urllib.request.urlopen(url).getcode())
+    except:
+        # If URL is dead repeat until working URL is found
+        repeat
